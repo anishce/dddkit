207e369aa3f92de2ba6d5007acee371916c69bdb
Anish <anish.cse7@gmail.com>
2024-04-27 22:48:30 +0530

    Added CoachingMentorDomain project
    Added CoachingMentorDomain project

---
+++ src/TrainingDomain/CoachingMentoring/CoachingMentoringDomain/CoachingContext/CoachAggregate/Coach.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoachingMentoringDomain.CoachingContext.CoachAggregate
+{
+    internal class Coach
+    {
+    }
+}
---
+++ src/TrainingDomain/CoachingMentoring/CoachingMentoringDomain/FeedbackAndReviewContext/ReviewAggregate/Review.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoachingMentoringDomain.FeedbackAndReviewContext.ReviewAggregate
+{
+    internal class Review
+    {
+    }
+}

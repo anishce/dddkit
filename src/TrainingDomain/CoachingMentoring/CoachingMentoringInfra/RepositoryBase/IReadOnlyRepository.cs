0706b91f6a68af682d85c1de63afa7979cdca3d8
Anish <anish.cse7@gmail.com>
2024-04-28 20:02:00 +0530

    Added Repository Base in Coaching Mentoring Infra Project
    Added Repository Base in Coaching Mentoring Infra Project

---
+++ src/TrainingDomain/CoachingMentoring/CoachingMentoringInfra/RepositoryBase/IReadOnlyRepository.cs
@@ -0,0 +1,14 @@
+﻿using CoachingMentoringInfra.DomainBase;
+using CoachingMentoringInfra.Querying;
+using System.Collections.Generic;
+
+namespace CoachingMentoringInfra.RepositoryBase
+{
+    public interface IReadOnlyRepository<T, TId> where T : IAggregateRoot 
+    {
+        T FindBy(TId id);
+        IEnumerable<T> FindAll();        
+        IEnumerable<T> FindBy(Query query);
+        IEnumerable<T> FindBy(Query query, int index, int count);
+    }
+}
---
+++ src/TrainingDomain/CoachingMentoring/CoachingMentoringInfra/RepositoryBase/RepositoryBase.cs
@@ -0,0 +1,44 @@
+﻿using CoachingMentoringInfra.DomainBase;
+using CoachingMentoringInfra.Querying;
+using CoachingMentoringInfra.UnitOfWork;
+using System.Collections.Generic;
+
+namespace CoachingMentoringInfra.RepositoryBase
+{
+    public abstract class RepositoryBase<T, TId> : IRepository<T, TId>, IUnitOfWorkRepository where T : IAggregateRoot
+    {
+        #region IRepository
+        public abstract void Add(T entity);
+        public abstract void Save(T entity);
+        public abstract void Remove(T entity);
+
+        public abstract T FindBy(TId id);
+        public abstract IEnumerable<T> FindBy(Query query);
+        public abstract IEnumerable<T> FindBy(Query query, int index, int count);
+        public abstract IEnumerable<T> FindAll();
+        #endregion
+
+        #region IUnitOfWorkRepository
+        public void PersistCreationOf(IAggregateRoot entity)
+        {
+            this.PersistNewItem((T)entity);
+        }
+
+        public void PersistDeletionOf(IAggregateRoot entity)
+        {
+            this.PersistDeletedItem((T)entity);
+        }
+
+        public void PersistUpdateOf(IAggregateRoot entity)
+        {
+            this.PersistUpdatedItem((T)entity);
+        }
+        #endregion
+
+        #region Abstract Method
+        protected abstract void PersistNewItem(T item);
+        protected abstract void PersistUpdatedItem(T item);
+        protected abstract void PersistDeletedItem(T item);
+        #endregion
+    }
+}

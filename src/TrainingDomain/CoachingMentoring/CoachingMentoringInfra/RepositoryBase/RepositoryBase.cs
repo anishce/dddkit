9b162842c8f2691e6b5a8cf20b47f4fd4e53db1f
Anish <anish.cse7@gmail.com>
2024-05-01 22:31:04 +0530

    Updated repository base
    Updated repository base

--- src/TrainingDomain/CoachingMentoring/CoachingMentoringInfra/RepositoryBase/RepositoryBase.cs
+++ src/TrainingDomain/CoachingMentoring/CoachingMentoringInfra/RepositoryBase/RepositoryBase.cs
@@ -1,11 +1,10 @@
 ﻿using CoachingMentoringInfra.DomainBase;
 using CoachingMentoringInfra.Querying;
-using CoachingMentoringInfra.UnitOfWork;
 using System.Collections.Generic;
 
 namespace CoachingMentoringInfra.RepositoryBase
 {
-    public abstract class RepositoryBase<T, TId> : IRepository<T, TId>, IUnitOfWorkRepository where T : IAggregateRoot
+    public abstract class RepositoryBase<T, TId> : IRepository<T, TId>  where T : IAggregateRoot
     {
         #region IRepository
         public abstract void Add(T entity);
@@ -16,29 +15,6 @@ namespace CoachingMentoringInfra.RepositoryBase
         public abstract IEnumerable<T> FindBy(Query query);
         public abstract IEnumerable<T> FindBy(Query query, int index, int count);
         public abstract IEnumerable<T> FindAll();
-        #endregion
-
-        #region IUnitOfWorkRepository
-        public void PersistCreationOf(IAggregateRoot entity)
-        {
-            this.PersistNewItem((T)entity);
-        }
-
-        public void PersistDeletionOf(IAggregateRoot entity)
-        {
-            this.PersistDeletedItem((T)entity);
-        }
-
-        public void PersistUpdateOf(IAggregateRoot entity)
-        {
-            this.PersistUpdatedItem((T)entity);
-        }
-        #endregion
-
-        #region Abstract Method
-        protected abstract void PersistNewItem(T item);
-        protected abstract void PersistUpdatedItem(T item);
-        protected abstract void PersistDeletedItem(T item);
         #endregion
     }
 }

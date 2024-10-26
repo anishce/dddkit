c34802455698493b3ff272c1bc7b666f30251686
Anish CE <anish.cse7@gmail.com>
2024-10-26 15:19:38 +0530

    Update Role.cs

--- src/TrainingDomain/CoachingMentoring/CoachingMentoringDomain/UserManagementContext/RoleAggregate/Role.cs
+++ src/TrainingDomain/CoachingMentoring/CoachingMentoringDomain/UserManagementContext/RoleAggregate/Role.cs
@@ -12,11 +12,11 @@ namespace CoachingMentoringDomain.UserManagementContext.RoleAggregate
             Initialize(name, remarks, createdBy, createdDate, modifiedBy, modifiedDate);
         }
 
-        public string Name { get; private set; }
-        public string Remarks { get; private set; }
-        public string CreatedBy { get; private set; }
+        public string Name { get; private set; } = null!;
+        public string? Remarks { get; private set; }
+        public string CreatedBy { get; private set; } = null!;
         public DateTime CreatedDate { get; private set; }
-        public string ModifiedBy { get; private set; }
+        public string ModifiedBy { get; private set; } = null!;
         public DateTime? ModifiedDate { get; private set; }
 
         protected override void ValidateEntity()

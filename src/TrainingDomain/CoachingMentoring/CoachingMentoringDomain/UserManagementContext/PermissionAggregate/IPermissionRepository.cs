d5d873dc729eb9f9f86eff049689b2393ca9e046
Anish CE <anish.cse7@gmail.com>
2024-11-03 19:02:21 +0530

    Create IPermissionRepository.cs

---
+++ src/TrainingDomain/CoachingMentoring/CoachingMentoringDomain/UserManagementContext/PermissionAggregate/IPermissionRepository.cs
@@ -0,0 +1,13 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using CoachingMentoringInfra.RepositoryBase;
+
+namespace CoachingMentoringDomain.UserManagementContext.PermissionAggregate
+{
+    public interface IPermissionRepository : IRepository<Permission, int>
+    {
+    }
+}

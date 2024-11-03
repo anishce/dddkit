a24b137c3cdffb37fb27bd5de762681543373267
Anish CE <anish.cse7@gmail.com>
2024-11-03 18:54:07 +0530

    Create IRoleFactory.cs

---
+++ src/TrainingDomain/CoachingMentoring/CoachingMentoringDomain/UserManagementContext/RoleAggregate/IRoleFactory.cs
@@ -0,0 +1,14 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using System;
+
+namespace CoachingMentoringDomain.UserManagementContext.RoleAggregate
+{
+    public interface IRoleFactory
+    {
+        Role CreateFrom(int id, string name, string remarks, string createdBy, DateTime createdDate, string modifiedBy, DateTime? modifiedDate, int version);
+    }
+}

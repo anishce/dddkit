12a050e4998912db3d5c66bb22362454800f4477
Anish CE <anish.cse7@gmail.com>
2024-11-03 11:48:20 +0530

    Update Coach.cs

--- src/TrainingDomain/CoachingMentoring/CoachingMentoringDomain/CoachingContext/CoachAggregate/Coach.cs
+++ src/TrainingDomain/CoachingMentoring/CoachingMentoringDomain/CoachingContext/CoachAggregate/Coach.cs
@@ -8,11 +8,21 @@ namespace CoachingMentoringDomain.CoachingContext.CoachAggregate
 {
     public class Coach : BaseEntity<int>, IAggregateRoot
     {
-        public Coach(int id, string firstName, string lastName, string email, string phone, 
+        public Coach(int id, string firstName, string middleName, string lastName, string email, string phone, 
             IEnumerable<Specialization> specializations, IEnumerable<Certification> certifications, int experience, string bio, double rating, int version) :base(id,version)
         {
-            
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
         }
+
+        public string FirstName { get; private set; } = null!;
+        public string? MiddleName { get; private set; }
+        public string LastName { get; private set; } = null!;
+        public string Email { get; private set; } = null!;
+        public string? PhoneNumber { get; private set; }
+        public IEnumerable<Specialization> Specializations { get; private set; } =null!;
+        public IEnumerable<Certification> Certifications { get; private set; } = null!;
 
         protected override void ValidateEntity()
         {

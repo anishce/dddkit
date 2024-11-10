aea0a28100b37d67202211e8aabc19786f797170
Anish CE <anish.cse7@gmail.com>
2024-11-10 15:29:43 +0530

    Create IPermissionFactory.cs

---
+++ src/TrainingDomain/CoachingMentoring/CoachingMentoringDomain/UserManagementContext/PermissionAggregate/IPermissionFactory.cs
@@ -0,0 +1,14 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using System;
+
+namespace CoachingMentoringDomain.UserManagementContext.PermissionAggregate
+{
+    public interface IPermissionFactory
+    {
+        Permission CreateFrom(int id, string name, string description, string createdBy, DateTime createdDate, string modifiedBy, DateTime? modifiedDate, int version);
+    }
+}

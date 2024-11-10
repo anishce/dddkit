a4a8c7b4db1d7bdbc1647e561ccb0ba76fd6952f
Anish CE <anish.cse7@gmail.com>
2024-11-10 15:29:50 +0530

    Create PermissionFactory.cs

---
+++ src/TrainingDomain/CoachingMentoring/CoachingMentoringDomain/UserManagementContext/PermissionAggregate/PermissionFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CoachingMentoringDomain.UserManagementContext.PermissionAggregate
+{
+    public class PermissionFactory : IPermissionFactory
+    {
+        public Permission CreateFrom(int id, string name, string description,
+            string createdBy, DateTime createdDate, string modifiedBy, DateTime? modifiedDate, int version)
+        {
+            return new Permission(id, name, description, createdBy, createdDate, modifiedBy, modifiedDate, version);
+        }
+    }
+}

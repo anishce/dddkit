15604a445be7370a9b59df97ece4193797ffe07c
Anish CE <anish.cse7@gmail.com>
2024-11-10 15:37:44 +0530

    Create ICoachRepository.cs

---
+++ src/TrainingDomain/CoachingMentoring/CoachingMentoringDomain/CoachingContext/CoachAggregate/ICoachRepository.cs
@@ -0,0 +1,13 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using CoachingMentoringInfra.RepositoryBase;
+
+namespace CoachingMentoringDomain.CoachingContext.CoachAggregate
+{
+    internal interface ICoachRepository : IRepository<Coach,int>
+    {
+    }
+}

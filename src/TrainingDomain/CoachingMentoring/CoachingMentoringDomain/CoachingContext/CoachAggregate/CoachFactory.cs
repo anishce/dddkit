b764b2c5ade63b3c8cf448efdde31f9497d2082b
Anish CE <anish.cse7@gmail.com>
2024-11-10 15:37:22 +0530

    Create CoachFactory.cs

---
+++ src/TrainingDomain/CoachingMentoring/CoachingMentoringDomain/CoachingContext/CoachAggregate/CoachFactory.cs
@@ -0,0 +1,20 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+// Ignore Spelling: bio
+
+using System.Collections.Generic;
+
+namespace CoachingMentoringDomain.CoachingContext.CoachAggregate
+{
+    public class CoachFactory : ICoachFactory
+    {
+        public Coach CreateFrom(int id, string firstName, string middleName, string lastName, string email, string phone,
+            IEnumerable<Specialization> specializations, IEnumerable<Certification> certifications, int experience, string bio, double rating, int version)
+        {
+            return new Coach(id, firstName, middleName, lastName, email, phone, specializations, certifications, experience, bio, rating, version);
+        }
+    }
+}

4cae2d264b9213d79cfee159a9e6e7207f37255c
Anish CE <anish.cse7@gmail.com>
2024-11-30 19:02:54 +0530

    Create FeedBackType.cs

---
+++ src/TrainingDomain/CoachingMentoring/CoachingMentoringDomain/FeedbackAndReviewContext/FeedbackAggregate/FeedBackType.cs
@@ -0,0 +1,15 @@
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+namespace CoachingMentoringDomain.FeedbackAndReviewContext.FeedbackAggregate
+{
+    public enum FeedBackType
+    {
+        Coaching = 1,
+        Mentoring = 2,
+        Session = 3,
+        Performance = 4
+    }
+}

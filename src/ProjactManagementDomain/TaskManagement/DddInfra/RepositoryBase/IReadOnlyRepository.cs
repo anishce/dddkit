61752cdce1bab4f1f99f72e0e264a48543847c19
Anish CE <anish.cse7@gmail.com>
2024-12-07 17:37:09 +0530

    Added Ddd infra project
    Added Ddd infra project

---
+++ src/ProjactManagementDomain/TaskManagement/DddInfra/RepositoryBase/IReadOnlyRepository.cs
@@ -0,0 +1,14 @@
+﻿using AnishCeDev.Ddd.Infra.DomainBase;
+using AnishCeDev.Ddd.Infra.Querying;
+using System.Collections.Generic;
+
+namespace AnishCeDev.Ddd.Infra.RepositoryBase
+{
+    public interface IReadOnlyRepository<T, TId> where T : IAggregateRoot 
+    {
+        T FindBy(TId id);
+        IEnumerable<T> FindAll();        
+        IEnumerable<T> FindBy(Query query);
+        IEnumerable<T> FindBy(Query query, int index, int count);
+    }
+}

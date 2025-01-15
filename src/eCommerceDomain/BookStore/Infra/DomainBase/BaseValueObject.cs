2d18773cb738b16fbcac29e78208f919497ae51f
Anish CE <anish.cse7@gmail.com>
2025-01-15 21:32:59 +0530

    Update BaseValueObject.cs

--- src/eCommerceDomain/BookStore/Infra/DomainBase/BaseValueObject.cs
+++ src/eCommerceDomain/BookStore/Infra/DomainBase/BaseValueObject.cs
@@ -1,12 +1,17 @@
-﻿using System.Collections.Generic;
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using System.Collections.Generic;
 
 namespace AnishCeDev.Ddd.Infra.DomainBase
 {
-    public abstract class BaseValueObject<T> where T: BaseValueObject<T>
+    public abstract class BaseValueObject<T> where T : BaseValueObject<T>
     {
         public BaseValueObject()
         {
-        
+
         }
 
         protected abstract IEnumerable<object> GetAttributesToBeCheckForEquality();

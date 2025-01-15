1c5666c0d241985cd9358f1d9c2ecd7c9724698e
Anish CE <anish.cse7@gmail.com>
2025-01-15 21:32:48 +0530

    Update Class1.cs

--- src/eCommerceDomain/BookStore/AnishCeDev.BookStore.Domain/Class1.cs
+++ src/eCommerceDomain/BookStore/AnishCeDev.BookStore.Domain/Class1.cs
@@ -1,4 +1,9 @@
-﻿namespace AnishCeDev.BookStore.Domain
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+namespace AnishCeDev.BookStore.Domain
 {
     public class Class1
     {

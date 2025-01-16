472cee2a5dc573a596e8d4859ac13c41048b5194
Anish CE <anish.cse7@gmail.com>
2025-01-16 21:41:54 +0530

    Update Coach.cs

--- src/TrainingDomain/CoachingMentoring/CoachingMentoringDomain/CoachingContext/CoachAggregate/Coach.cs
+++ src/TrainingDomain/CoachingMentoring/CoachingMentoringDomain/CoachingContext/CoachAggregate/Coach.cs
@@ -1,4 +1,7 @@
-﻿// Ignore Spelling: bio
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
 
 using CoachingMentoringInfra.DomainBase;
 using System;
@@ -8,8 +11,8 @@ namespace CoachingMentoringDomain.CoachingContext.CoachAggregate
 {
     public class Coach : BaseEntity<int>, IAggregateRoot
     {
-        public Coach(int id, string firstName, string middleName, string lastName, string email, string phone, 
-            IEnumerable<Specialization> specializations, IEnumerable<Certification> certifications, int experience, string bio, double rating, int version) :base(id,version)
+        public Coach(int id, string firstName, string middleName, string lastName, string email, string phone,
+            IEnumerable<Specialization> specializations, IEnumerable<Certification> certifications, int experience, string bio, double rating, int version) : base(id, version)
         {
             FirstName = firstName;
             MiddleName = middleName;
@@ -21,7 +24,7 @@ namespace CoachingMentoringDomain.CoachingContext.CoachAggregate
         public string LastName { get; private set; } = null!;
         public string Email { get; private set; } = null!;
         public string? PhoneNumber { get; private set; }
-        public IEnumerable<Specialization> Specializations { get; private set; } =null!;
+        public IEnumerable<Specialization> Specializations { get; private set; } = null!;
         public IEnumerable<Certification> Certifications { get; private set; } = null!;
 
         protected override void ValidateEntity()

c633796b52084e4ae33e4443855598f3532b02e1
Anish CE <anish.cse7@gmail.com>
2025-01-18 15:25:35 +0530

    Update IUserFactory.cs

--- src/TrainingDomain/CoachingMentoring/CoachingMentoringDomain/UserManagementContext/UserAggregate/IUserFactory.cs
+++ src/TrainingDomain/CoachingMentoring/CoachingMentoringDomain/UserManagementContext/UserAggregate/IUserFactory.cs
@@ -1,4 +1,8 @@
-﻿
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
 using System;
 using System.Collections.Generic;
 
@@ -6,7 +10,7 @@ namespace CoachingMentoringDomain.UserManagementContext.UserAggregate
 {
     public interface IUserFactory
     {
-        User CreateFrom(int id, string firstName, string middleName, string lastName, string userName, string password, int roleId, UserContact contact, IEnumerable<UserAddress> addresses, 
+        User CreateFrom(int id, string firstName, string middleName, string lastName, string userName, string password, int roleId, UserContact contact, IEnumerable<UserAddress> addresses,
             bool isActive, string remarks, string createdBy, DateTime createdDate, string modifiedBy, DateTime? modifiedDate, int version);
     }
 }

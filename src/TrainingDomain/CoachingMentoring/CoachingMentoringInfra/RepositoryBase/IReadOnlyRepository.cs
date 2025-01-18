208f46c77e3d522f54d37dab1db536426ec41097
Anish CE <anish.cse7@gmail.com>
2025-01-18 15:26:25 +0530

    Update IReadOnlyRepository.cs

--- src/TrainingDomain/CoachingMentoring/CoachingMentoringInfra/RepositoryBase/IReadOnlyRepository.cs
+++ src/TrainingDomain/CoachingMentoring/CoachingMentoringInfra/RepositoryBase/IReadOnlyRepository.cs
@@ -1,13 +1,18 @@
-﻿using CoachingMentoringInfra.DomainBase;
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using CoachingMentoringInfra.DomainBase;
 using CoachingMentoringInfra.Querying;
 using System.Collections.Generic;
 
 namespace CoachingMentoringInfra.RepositoryBase
 {
-    public interface IReadOnlyRepository<T, TId> where T : IAggregateRoot 
+    public interface IReadOnlyRepository<T, TId> where T : IAggregateRoot
     {
         T FindBy(TId id);
-        IEnumerable<T> FindAll();        
+        IEnumerable<T> FindAll();
         IEnumerable<T> FindBy(Query query);
         IEnumerable<T> FindBy(Query query, int index, int count);
     }

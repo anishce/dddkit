2319d094b0e0f1933f505b399a590ce18583b85b
Anish CE <anish.cse7@gmail.com>
2025-01-18 15:26:31 +0530

    Update RepositoryBase.cs

--- src/TrainingDomain/CoachingMentoring/CoachingMentoringInfra/RepositoryBase/RepositoryBase.cs
+++ src/TrainingDomain/CoachingMentoring/CoachingMentoringInfra/RepositoryBase/RepositoryBase.cs
@@ -1,10 +1,15 @@
-﻿using CoachingMentoringInfra.DomainBase;
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using CoachingMentoringInfra.DomainBase;
 using CoachingMentoringInfra.Querying;
 using System.Collections.Generic;
 
 namespace CoachingMentoringInfra.RepositoryBase
 {
-    public abstract class RepositoryBase<T, TId> : IRepository<T, TId>  where T : IAggregateRoot
+    public abstract class RepositoryBase<T, TId> : IRepository<T, TId> where T : IAggregateRoot
     {
         #region IRepository
         public abstract void Add(T entity);

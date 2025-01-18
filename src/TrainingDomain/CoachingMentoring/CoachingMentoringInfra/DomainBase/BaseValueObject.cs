b52b50895f56e38a0c79b570bd3f978d3ad65ef7
Anish CE <anish.cse7@gmail.com>
2025-01-18 15:26:14 +0530

    Update BaseValueObject.cs

--- src/TrainingDomain/CoachingMentoring/CoachingMentoringInfra/DomainBase/BaseValueObject.cs
+++ src/TrainingDomain/CoachingMentoring/CoachingMentoringInfra/DomainBase/BaseValueObject.cs
@@ -1,12 +1,17 @@
-﻿using System.Collections.Generic;
+﻿// ************************************************************************
+// Copyright (c) AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using System.Collections.Generic;
 
 namespace CoachingMentoringInfra.DomainBase
 {
-    public abstract class BaseValueObject<T> where T: BaseValueObject<T>
+    public abstract class BaseValueObject<T> where T : BaseValueObject<T>
     {
         public BaseValueObject()
         {
-        
+
         }
 
         protected abstract IEnumerable<object> GetAttributesToBeCheckForEquality();

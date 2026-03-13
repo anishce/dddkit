2fe9b4d78dadcf36655e7a18a9b167197cbe9500
Anish CE <anish.cse7@gmail.com>
2026-03-13 18:48:03 +0530

    Create BookEditionEnum.cs

---
+++ src/eCommerceDomain/BookStore/AnishCeDev.BookStore.Domain/BookAggregate/BookEditionEnum.cs
@@ -0,0 +1,27 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+namespace AjSys.DigitalBookShop.BookCatalog.Domain.BookAggregate
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum BookEdition
+    {
+        [Display(Name = "First Edition")]
+        FirstEdition = 1,
+
+        [Display(Name = "Second Edition")]
+        SecondEdition = 2,
+
+        [Display(Name = "Revised Edition")]
+        RevisedEdition = 3,
+
+        [Display(Name = "Special Edition")]
+        SpecialEdition = 4,
+
+        [Display(Name = "Collector's Edition")]
+        CollectorsEdition = 5
+    }
+
+}

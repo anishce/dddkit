2fc4042fab6bb05663f6f92ff74a7fcbde0f3cfd
Anish CE <anish.cse7@gmail.com>
2026-03-13 18:53:54 +0530

    Create IBookRepository.cs

---
+++ src/eCommerceDomain/BookStore/AnishCeDev.BookStore.Domain/BookAggregate/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using AjSys.DigitalBookShop.BookCatalog.Infrastructure.RepositoryBase;
+
+namespace AnishCeDev.BookStore.Domain.BookAggregate
+{
+    public interface IBookRepository : IRepository<Book, int>
+    {
+    }
+}

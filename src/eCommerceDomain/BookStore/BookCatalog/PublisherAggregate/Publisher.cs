36cda613d7d14af5da5b6857df937194c7b5f198
Anish CE <anish.cse7@gmail.com>
2026-04-26 09:48:10 +0530

    Create Publisher.cs

---
+++ src/eCommerceDomain/BookStore/BookCatalog/PublisherAggregate/Publisher.cs
@@ -0,0 +1,28 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using AjSys.DigitalBookShop.Infrastructure.DomainBase;
+using System;
+
+namespace AnishCeDev.BookStore.Domain.PublisherAggregate
+{
+    public class Publisher : EntityBase<Guid>, IAggregateRoot
+    {
+        public Publisher(Guid id, string name, int version) : base(id,version)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+
+        protected override void ValidateEntity()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                base.AddBrokenValidationRule("A book category must have name.");
+            }
+        }
+    }
+}

d13b34862126d857efa750a0d532e949c3d7fc20
Anish CE <anish.cse7@gmail.com>
2026-04-26 09:48:06 +0530

    Create CategoryBusinessRules.cs

---
+++ src/eCommerceDomain/BookStore/BookCatalog/CategoryAggregate/CategoryBusinessRules.cs
@@ -0,0 +1,14 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+using AnishCeDev.Ddd.Infra.DomainBase;
+
+namespace AnishCeDev.BookStore.Domain.CategoryAggregate
+{
+    internal class CategoryBusinessRules
+    {
+        public static readonly BusinessRule NameRequired = new BusinessRule(nameof(Category.Name), "A book category must have name.");
+    }
+}

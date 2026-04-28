5b0dd247b78ceb3526be0d4ebb191a6fc7d0c5a8
Anish CE <anish.cse7@gmail.com>
2026-04-28 10:34:31 +0530

    Create LocationEnum.cs

---
+++ src/eCommerceDomain/BookStore/BookCatalog/StockAggregate/LocationEnum.cs
@@ -0,0 +1,11 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+public enum LocationType
+{
+    Warehouse = 0,
+    Store = 1,
+    FulfillmentCenter = 2
+}

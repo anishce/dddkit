26d2b2228b92af043905ded59233b857337ccb2f
Anish CE <anish.cse7@gmail.com>
2026-04-28 10:35:21 +0530

    Create Class1.cs

---
+++ src/eCommerceDomain/BookStore/OrderManagement/Class1.cs
@@ -0,0 +1,12 @@
+﻿// ************************************************************************
+// Copyright (c) 2025 AnishCeDev All Rights Reserved.
+// Author: AnishCeDev
+// ************************************************************************
+
+namespace AnishCeDev.BookStore.OrderManagement.Domain
+{
+    public class Class1
+    {
+
+    }
+}
